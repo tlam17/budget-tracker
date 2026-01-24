63cfd4feb21900abff3fd4a5e71376bee8b1c19c
tlam17 <94070835+tlam17@users.noreply.github.com>
2026-01-24 17:58:32 -0600

    Created User entity

---
+++ budget-tracker-api/src/main/java/com/tylerlam/budgettracker/budget_tracker_api/user/User.java
@@ -0,0 +1,59 @@
+package com.tylerlam.budgettracker.budget_tracker_api.user;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(
+    name = "users", 
+    indexes = {
+        @Index(name = "idx_user_email", columnList = "email")
+    },
+    uniqueConstraints = {
+        @UniqueConstraint(name = "ux_user_email", columnNames = "email")
+    }
+)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, name = "first_name", length = 100)
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name", length = 100)
+    private String lastName;
+
+    @Column(nullable = false, name = "password_hash")
+    private String passwordHash;
+
+    @Column(nullable = false, name = "base_currency", length = 3)
+    private String baseCurrency = "USD";
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
