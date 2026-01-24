d94e967640c26281196366609f3deee828bd82e3
tlam17 <94070835+tlam17@users.noreply.github.com>
2026-01-24 14:11:33 -0600

    Created backend skeleton

---
+++ budget-tracker-api/src/main/java/com/tylerlam/budgettracker/budget_tracker_api/BudgetTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tylerlam.budgettracker.budget_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerApiApplication.class, args);
+	}
+
+}
---
+++ budget-tracker-api/src/test/java/com/tylerlam/budgettracker/budget_tracker_api/BudgetTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerlam.budgettracker.budget_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
